01e6fe00c536d38c6d6bf6960a375ec04a6c1704
ybkim-dev <beanybigggy@gmail.com>
2022-02-01 20:18:56 +0900

    [feat] : theater controller 추가

--- src/main/java/com/youngbin/controller/TheaterController.java
+++ src/main/java/com/youngbin/controller/TheaterController.java
@@ -1,12 +1,21 @@
 package com.youngbin.controller;
 
 import com.youngbin.bo.TheaterBO;
+import org.codehaus.jettison.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class TheaterController {
     @Autowired
     TheaterBO theaterBO;
 
+    @RequestMapping(value = "theaters", method = RequestMethod.POST)
+    public void insertTheatersByCrawling() throws IOException, JSONException {
+        theaterBO.insertTheaterByCrawling();
+    }
 }
